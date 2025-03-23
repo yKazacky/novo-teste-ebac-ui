6606c250bce12e41ecd2936538ba904312469a39
Kazacky <lucasacky@protonmail.com>
2025-03-23 07:11:33 -0300

    Testes utilizando Page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,17 +1,32 @@
 /// <reference types="cypress" />
+import produtosPage from "../../support/page-objects/produtos.page";
 
 
 describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
-    
-    
+
     it('Deve selecionar um produto da lista', () => {
         cy.get('.products > .row')
         .contains('Apollo Running Short')
         .click()
 
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProdutoLista('Apollo Running Short')
+    });
+
+    it.only('Deve visitar a pagina do produto', () => {
+        let produto = 'Kratos Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+        
+    });
+
+    it('Deve adicionar o produto ao carrinho', () => {
+        
     });
 });
